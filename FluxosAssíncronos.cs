1813e9e898f4bd08d03510756e50356e5b83b8eb
Thiago de Melo Lima <Thiago_badboy300@hotmail.com>
2020-06-20 21:06:56 -0300

    Fluxos Assíncronos

---
+++ FluxosAssíncronos.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpNewVersion
+{
+    class FluxosAssíncronos
+    {
+        public async IAsyncEnumerable<int> GetNumbers()
+        {
+            for(int i = 0; i<20;i++)
+            {
+                await Task.Delay(100);
+                yield return i;
+            }
+        }
+
+        [Test]
+        public async Task FluxosAssíncronosTest()
+        {
+            await foreach(var number in GetNumbers())
+            {
+                Debug.WriteLine(number);
+            }
+        }
+    }
+}
