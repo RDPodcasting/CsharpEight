9efc82e4bfef0bb233666e30815c5ea00dd96859
Thiago de Melo Lima <Thiago_badboy300@hotmail.com>
2020-06-21 22:53:11 -0300

    Finish Win

---
+++ PadroesDaPropriedade.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpNewVersion
+{
+    class Address
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+    class PadroesDaPropriedade
+    {
+        public decimal GetTax(Address address) =>
+            address switch
+            {
+                { State: "SP" } => 10,
+                { State: "PR" } => 13,
+                _ => 5
+            };
+
+        [Test]
+        public void PadroesDaPropriedadeTeste()
+        {
+            var firstAddress = new Address() { State = "SP" };
+
+            var secondAddress = new Address() { State = "PR" };
+
+            var thirdAddress = new Address() { State = "MG" };
+
+            var firstTax = GetTax(firstAddress);
+            var secondTax = GetTax(secondAddress);
+            var thirdTax = GetTax(thirdAddress);
+
+            Assert.That(firstTax, Is.EqualTo(10));
+            Assert.That(secondTax, Is.EqualTo(13));
+            Assert.That(thirdTax, Is.EqualTo(5));
+
+
+
+
+
+        }
+    }
+}
---
+++ PadroesDeTupla.cs
@@ -0,0 +1,29 @@
+﻿
+using NUnit.Framework;
+
+namespace CsharpNewVersion
+{
+    class PadroesDeTupla
+    {
+        public decimal GetTax(string state, string city) =>
+            (state, city) switch
+            {
+                ( "SP", "Sorocaba" ) => 10,
+                ( "SP", "Ourinhos" ) => 12,
+                ( "PR", "Londrina" ) => 13,
+                ( _, _ ) => 5
+            };
+
+        [Test]
+        public void PadroesDeTuplaTest()
+        {
+            var firstTax = GetTax("SP", "Sorocaba");
+            var secondTax = GetTax("SP", "Ourinhos");
+            var thirdTax = GetTax("PR", "Londrina");
+
+            Assert.That(firstTax, Is.EqualTo(10));
+            Assert.That(secondTax, Is.EqualTo(12));
+            Assert.That(thirdTax, Is.EqualTo(13));
+        }
+    }
+}
